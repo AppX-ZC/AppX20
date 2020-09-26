ef6aef0ffa0e7d8d1736a3341dfbdf2f4b681ea5
salah taha <salahtaha065@gmail.com>
2020-09-26 21:00:45 +0200

    finish during

--- src/sections/during_the_hackathon/During.js
+++ src/sections/during_the_hackathon/During.js
@@ -12,6 +12,31 @@ function During() {
         a number of challenges under the Sustainable Development Goals:
       </p>
       <img src="images/SDGs.png" alt="" className="during__image" />
+      <div className="during__days">
+        <a
+          href="https://docs.google.com/document/d/1znP51mL9emoMuUMf-UCjjkkzuAJuoYGZB8rdyXWrK8c/edit#heading=h.a957cpk70uyg"
+          className="during__days__day"
+          target="_blank"
+        >
+          Day 1
+        </a>
+
+        <a
+          href="https://docs.google.com/document/d/13hkAecqVtnvyYWbuBKqRVuNdz3BsRrGdxZ9U0J0PKeo/edit#"
+          className="during__days__day"
+          target="_blank"
+        >
+          Day 2
+        </a>
+
+        <a
+          href="https://docs.google.com/document/d/1QDnaTZN4bnvqr0Qbw4TgqqrPhL-t1VhH8dJznVskGVQ/edit#heading=h.knxsib7wzvm1"
+          className="during__days__day"
+          target="_blank"
+        >
+          Day 3
+        </a>
+      </div>
     </div>
   );
 }
